bfce2103f6044d24eda0388d5156e574191b11ff
yisus.gr <guevarayisus1@gmail.com>
2023-10-25 19:02:03 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+
+const port = 4000
+
+app.set('port', port)
+
+app.use(myconn(mysql, {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'bibliotech'
+}))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.use('/api/libros', require('./routes/libros'))
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port  ${app.get('port')}`)
+})
---
+++ src/routes/libros.js
@@ -0,0 +1,121 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+
+    req.getConnection((err, conn) => {
+
+      if (err) {
+        return res.status(500).send("error en el servidor");
+      }
+
+      conn.query("SELECT * FROM catalogo", (err, rows) => {
+
+        if (err) {
+          return res.status(500).send("error en el servidor");
+        }
+
+        res.json(rows);
+      });
+
+    });
+    
+});
+
+router.get("/:id", (req, res) => {
+
+    req.getConnection((err, conn) => {
+      if (err) {
+        return res.status(500).send("Error en el servidor ");
+      }
+
+        conn.query(
+            "SELECT * FROM catalogo WHERE ISBN = ?",
+            [req.params.id],
+            (err, result) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                res.json(result);
+            }
+        );
+
+    });
+
+});
+
+
+router.post("/", (req, res) => {
+    const { ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias } = req.body;
+
+    console.log(req.body);
+    
+    if (ISBN && titulo && autor && year && ID_biblioteca && portada_url && existencias) {
+      
+      req.getConnection((err, conn) => {
+        if (err) {
+          return res.status(500).send("Error en el servidor");
+        }
+        conn.query(
+          "INSERT INTO catalogo SET ?",
+          [{ ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias}],
+          (err) => {
+            if (err) {
+              console.log(err)
+              return res.status(500).send("error en el servidor");
+            }
+          }
+        );
+      });
+      res.json({ ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias});
+    } else {
+      res.json({ error: "there was an error" });
+      //console.log(err)
+    }
+});
+
+router.delete("/:isbn", (req, res) => {
+    req.getConnection((err, conn) => {
+      if (err) {
+        return res.status(500).send("error en el servidor");
+      }
+      conn.query("DELETE FROM catalogo WHERE isbn = ?", [req.params.isbn], (err) => {
+        if (err) {
+          return res.send(500).send("error en el servidor");
+        }
+        res.send("deleted");
+      });
+    });
+});
+
+router.put("/:isbn",  (req, res) => {
+    req.getConnection((err, conn) => {
+      if (err) {
+        return res.status(500).send("error en el servidor");
+      }
+      
+      const { titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias  } = req.body;
+      
+      console.log(req.body)
+
+      const updatedProducto = {
+        titulo, 
+        autor, 
+        year, 
+        sinopsis, 
+        ID_biblioteca, 
+        portada_url, 
+        existencias 
+      };
+      const isbn = req.params.isbn
+      conn.query("UPDATE catalogo SET ? WHERE isbn = ?", 
+      [updatedProducto, isbn], (err) =>{
+          if(err) {
+              return res.status(500).send('error en el servidor');
+          } 
+          res.send('updated')
+      });
+    });
+});
+
+module.exports = router;
