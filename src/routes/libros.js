1eab1b4ef05c4f55985f9f6ff7925cb6a273da48
yisus.gr <guevarayisus1@gmail.com>
2023-11-20 17:36:16 -0600

    Update apis

--- src/routes/libros.js
+++ src/routes/libros.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
         return res.status(500).send("error en el servidor");
       }
 
-      conn.query("SELECT * FROM catalogo", (err, rows) => {
+      conn.query("SELECT * FROM libros", (err, rows) => {
 
         if (err) {
           return res.status(500).send("error en el servidor");
@@ -30,13 +30,13 @@ router.get("/:id", (req, res) => {
       }
 
         conn.query(
-            "SELECT * FROM catalogo WHERE ISBN = ?",
+            "SELECT * FROM libros WHERE id = ?",
             [req.params.id],
             (err, result) => {
                 if (err) {
                     return res.status(500).send(err);
                 }
-                res.json(result);
+                res.json(result[0]);
             }
         );
 
@@ -46,40 +46,40 @@ router.get("/:id", (req, res) => {
 
 
 router.post("/", (req, res) => {
-    const { ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias } = req.body;
+    const { ISBN, titulo, autor, year, sinopsis, portada_url, categoria_id, editorial } = req.body;
 
     console.log(req.body);
     
-    if (ISBN && titulo && autor && year && ID_biblioteca && portada_url && existencias) {
+    if (ISBN && titulo && autor && year && portada_url && categoria_id && editorial ) {
       
       req.getConnection((err, conn) => {
         if (err) {
           return res.status(500).send("Error en el servidor");
         }
         conn.query(
-          "INSERT INTO catalogo SET ?",
-          [{ ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias}],
+          "INSERT INTO libros SET ?",
+          [{ ISBN, titulo, autor, year, sinopsis, portada_url, categoria_id, editorial}],
           (err) => {
             if (err) {
               console.log(err)
-              return res.status(500).send("error en el servidor");
+              return res.status(500).send("error AL INSERTAR");
             }
           }
         );
       });
-      res.json({ ISBN, titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias});
+      res.json({ ISBN, titulo, autor, year, sinopsis, portada_url, categoria_id, editorial});
     } else {
-      res.json({ error: "there was an error" });
+      res.json({ error: "DATOS FALTANTES" });
       //console.log(err)
     }
 });
 
-router.delete("/:isbn", (req, res) => {
+router.delete("/:id", (req, res) => {
     req.getConnection((err, conn) => {
       if (err) {
         return res.status(500).send("error en el servidor");
       }
-      conn.query("DELETE FROM catalogo WHERE isbn = ?", [req.params.isbn], (err) => {
+      conn.query("DELETE FROM libros WHERE id = ?", [req.params.id], (err) => {
         if (err) {
           return res.send(500).send("error en el servidor");
         }
@@ -88,32 +88,69 @@ router.delete("/:isbn", (req, res) => {
     });
 });
 
-router.put("/:isbn",  (req, res) => {
+router.put("/:id",  (req, res) => {
     req.getConnection((err, conn) => {
       if (err) {
         return res.status(500).send("error en el servidor");
       }
       
-      const { titulo, autor, year, sinopsis, ID_biblioteca, portada_url, existencias  } = req.body;
+      const { ISBN, titulo, autor, year, sinopsis, portada_url, categoria_id, editorial } = req.body;
       
       const updatedProducto = {
+        ISBN, 
         titulo, 
         autor, 
         year, 
         sinopsis, 
-        ID_biblioteca, 
-        portada_url, 
-        existencias 
+        portada_url,
+         categoria_id,
+         editorial     
       };
-      const isbn = req.params.isbn
-      conn.query("UPDATE catalogo SET ? WHERE isbn = ?", 
-      [updatedProducto, isbn], (err) =>{
+      const id = req.params.id
+      conn.query("UPDATE libros SET ? WHERE id = ?", 
+      [updatedProducto, id], (err) =>{
           if(err) {
               return res.status(500).send('error en el servidor');
           } 
           res.send('updated')
       });
-    });
+    }); 
 });
+
+router.get("/:id/reviews", (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err){
+      return res.status(500).send("Error en el servidor")
+    }
+    conn.query("SELECT * FROM REVIEWS WHERE ID_PUBLICACION = ?", 
+    [req.params.id], (err) =>{
+      if(err) {
+        return res.status(500).send("error al obtener datos")
+      }
+    })
+
+  })
+})
+
+router.post("/:id/reviews", (req, res) => {
+  req.getConnection((err, conn) => {
+    if(err){
+      return res.status(500).send("Error en el servidor")
+    }
+    const {ID_PUBLICACION, ID_USER, CONTENIDO, CALIFICACION} = req.params
+    if(ID_PUBLICACION && ID_USER && CONTENIDO && CALIFICACION){
+      const newReview = {
+        ID_PUBLICACION,
+        ID_USER,
+        CONTENIDO,
+        CALIFICACION
+      }
+      conn.query("INSTER INTO REVIEWS SET ?", [newReview])
+    } else {
+      return res.status(500).send("Error, datos faltantes")
+    }
+    
+  })
+})
 
 module.exports = router;
